e86c6373a38b56d72b952381dc1f89511d2bfec1
Phen-Reaksmey <reaksmeyphen777@gmail.com>
2025-11-26 10:05:24 +0700

    commit from Yeng/dev

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,8 @@ class DatabaseSeeder extends Seeder
         ]);
 
         // call news seeder
-        $this->call (NewsSeeder::class);
+        // $this->call (NewsSeeder::class);
+
+        
     }
 }
